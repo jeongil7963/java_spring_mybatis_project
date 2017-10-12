4b708d7d70031695ff056ae32d27d0d8b4072daa
양정일 <ya63kr@nate.com>
2017-10-12 11:10:18 +0900

    feat project

---
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -0,0 +1,39 @@
+package com.springbook.biz.board.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.util.SqlSessionFactoryBean;
+
+public class BoardDAO {
+    private SqlSession mybatis;
+
+    public BoardDAO() {
+        mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
+    }
+
+    public void insertBoard(BoardVO vo) {
+        mybatis.insert("Board.insertBoard", vo);
+        mybatis.commit();
+    }
+
+    public void updateBoard(BoardVO vo) {
+        mybatis.update("Board.updateBoard", vo);
+        mybatis.commit();
+    }
+
+    public void deleteBoard(BoardVO vo) {
+        mybatis.delete("Board.deleteBoard", vo);
+        mybatis.commit();
+    }
+
+    public BoardVO getBoard(BoardVO vo) {
+        return (BoardVO) mybatis.selectOne("Board.getBoard", vo);
+    }
+
+    public List<BoardVO> getBoardList(BoardVO vo) {
+        return mybatis.selectList("Board.getBoardList", vo);
+    }
+}
